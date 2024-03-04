ae964793b6019a286229734448bc1e9eb1c4d643
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-03-04 19:38:51 +0000

    feat: Add LoansController to LoansModule

--- src/modules/loans/loans.module.ts
+++ src/modules/loans/loans.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LoansService } from './loans.service';
+import { LoansController } from './loans.controller';
 
 @Module({
-  providers: [LoansService]
+  providers: [LoansService],
+  controllers: [LoansController]
 })
 export class LoansModule {}
