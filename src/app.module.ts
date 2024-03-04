5e408141e733984af6e962033bea4ae260ef433a
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-03-04 12:42:18 +0000

    chore: scaffold loans module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoansModule } from './modules/loans/loans.module';
 
 @Module({
-  imports: [],
+  imports: [LoansModule],
   controllers: [AppController],
   providers: [AppService],
 })
