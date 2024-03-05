072ba58dcb4fd63397ff00fc483112c38673f7cc
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-03-05 12:04:46 +0000

    feat: Refactor loans module imports and add HttpModule

--- src/modules/loans/loans.module.ts
+++ src/modules/loans/loans.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { LoansService } from './loans.service';
-import { LoansController } from './loans.controller';
+import { HttpModule } from '@nestjs/axios'
+import { Module } from '@nestjs/common'
+import { LoansController } from './loans.controller'
+import { LoansService } from './loans.service'
 
 @Module({
   providers: [LoansService],
-  controllers: [LoansController]
+  controllers: [LoansController],
+  imports: [HttpModule],
 })
 export class LoansModule {}
