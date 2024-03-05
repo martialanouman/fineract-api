0522513b7b279fb13e298152b896730ee9e2b803
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-03-05 12:27:35 +0000

    feat: Add loan repayment endpoint to loans controller

--- src/modules/loans/loans.controller.ts
+++ src/modules/loans/loans.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, Param, Post } from '@nestjs/common'
 import { LoanApplicationDto } from './dto/loan-application.dto'
 import { RepaymentDto } from './dto/repayment.dto'
 import { LoansService } from './loans.service'
@@ -7,13 +7,15 @@ import { LoansService } from './loans.service'
 export class LoansController {
   constructor(private service: LoansService) {}
 
-  @Post()
+  @Post('/')
   @HttpCode(201)
   createLoanApplication(@Body() data: LoanApplicationDto) {
     return this.service.createLoanApplication(data).catch((error) => error)
   }
 
-  createRepayment(loanId: number, data: RepaymentDto) {
+  @Post('/:loanId/repayments')
+  @HttpCode(201)
+  createRepayment(@Param('loanId') loanId: number, @Body() data: RepaymentDto) {
     return this.service.createRepayment(loanId, data).catch((error) => error)
   }
 }
