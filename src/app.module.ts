bc3bb7cb0ebd8b18d24edf40f7fe18a9af81cf47
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-03-05 12:04:27 +0000

    feat: Add ConfigModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module, ValidationPipe } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { APP_PIPE } from '@nestjs/core'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { LoansModule } from './modules/loans/loans.module'
 
 @Module({
-  imports: [LoansModule],
+  imports: [LoansModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService, { provide: APP_PIPE, useClass: ValidationPipe }],
 })
